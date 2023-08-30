f3924cce255613a210588b75fcb6aa0226c4afbd
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2023-08-29 23:53:24 -0300

    add dao factory interface

---
+++ Entregable1/src/DAOs/DAOFactory.java
@@ -0,0 +1,22 @@
+package DAOs;
+
+public interface DAOFactory {
+	public static final int MYSQL_JDBC = 1;
+	public static final int DERBY_JDBC = 2;
+	public static final int JPA_HIBERNATE = 3;
+
+	public abstract ClienteDAO getClienteDAO();
+
+	public abstract FacturaDAO getFacturaDAO();
+
+	public abstract ProductoDAO getProductoDAO();
+
+	public static DAOFactory getDAOFactory(int whichFactory) {
+		 switch (whichFactory) {
+		 case MYSQL_JDBC : return new MySqlDAOFactory();
+		 case DERBY_JDBC: return null;
+		 case JPA_HIBERNATE: return null;
+		 default: return null;
+		 }
+	}
+}
