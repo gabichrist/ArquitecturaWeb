bd7c6b07385f409ff3aff1c0c354f6bbc9a305cb
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2023-08-30 00:41:14 -0300

    update main

--- Entregable1/src/Application/Application.java
+++ Entregable1/src/Application/Application.java
@@ -1,11 +1,26 @@
 package Application;
 
-import DAOs.MySqlDAOFactory;
+import java.sql.SQLException;
+import java.util.List;
+
+import DAOs.ClienteDAO;
+import DAOs.DAOFactory;
+import entity.Cliente;
 
 public class Application {
 
 	public static void main(String[] args) {
-		MySqlDAOFactory d = new MySqlDAOFactory();
+		DAOFactory daoFactory = DAOFactory.getDAOFactory(DAOFactory.MYSQL_JDBC);
+		ClienteDAO daoClient = daoFactory.getClienteDAO();
+		try {
+			// TODO: test, borrar:
+			daoClient.addCliente(new Cliente(2, "segundo sombra", "segundo.gmx.net"));
+			List<Cliente> clientes = daoClient.getClientes();
+			clientes.forEach(cliente -> System.out.println(cliente));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
