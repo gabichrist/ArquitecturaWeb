850beca63f1e343c7ea14830ee3de21cb2d7f482
Magui <magalisuyaygarrido@gmail.com>
2023-09-21 18:51:18 -0300

    Se separo creacion y cierre de la conexion

--- Entregable2/src/application/Application.java
+++ Entregable2/src/application/Application.java
@@ -1,26 +1,33 @@
 package application;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
-import entity.Estudiante;
+import factory.ConnectionFactory;
+//import service.ServicioCarrera;
+//import service.ServicioEstudiante;
 
 public class Application {
 
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Entregable2");
-		EntityManager em = emf.createEntityManager();
 		
-		// Se crean y cargan estudiantes en la base de datos
+			
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		EntityManager em = connectionFactory.connection();
+		
+		
 		em.getTransaction().begin();
-		Estudiante e1 = new Estudiante(245852, "Juan", "Perez" , 25, "M", "39526555", "Tandil");
-		Estudiante e2 = new Estudiante(245953, "Maria", "Rodriguez" , 31, "F", "36222544", "Tandil");
-		em.persist(e1);
-		em.persist(e2);
+		
+		//ServicioEstudiante servicioEstudiante = new ServicioEstudiante();
+		//ServicioCarrera servicioCarrera = new ServicioCarrera();
+				
+		// Se crean y cargan estudiantes en la base de datos
+		//servicioEstudiante.altaEstudiante(null);
+	//	Estudiante e1 = new Estudiante(245852, "Juan", "Perez" , 25, "M", "39526555", "Tandil");
+		//Estudiante e2 = new Estudiante(245953, "Maria", "Rodriguez" , 31, "F", "36222544", "Tandil");
+		//em.persist(e1);
+		//em.persist(e2);
 		em.getTransaction().commit();
-		em.close();
-		emf.close();
+	
 	}
 
 }
