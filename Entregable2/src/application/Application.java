12d3849b32c51947c797e223ff51bf68a107e3af
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2023-09-22 21:44:27 -0300

    alta estudiante funcionando desde servicio
    falta verificar si ya existe registro con ese LU

--- Entregable2/src/application/Application.java
+++ Entregable2/src/application/Application.java
@@ -11,17 +11,16 @@ import entity.Estudiante;
 import entity.Inscripcion;
 import entity.InscripcionId;
 //import service.ServicioCarrera;
-//import service.ServicioEstudiante;
+import service.ServicioEstudiante;
 import factory.ConnectionFactory;
 
 public class Application {
 
 	public static void main(String[] args) {
 		
-		ConnectionFactory connectionFactory = new ConnectionFactory();
-		EntityManager em = connectionFactory.connection();
+		EntityManager em = ConnectionFactory.connection();
 		
-		//ServicioEstudiante servicioEstudiante = new ServicioEstudiante();
+		ServicioEstudiante servicioEstudiante = new ServicioEstudiante();
 	//	ServicioCarrera servicioCarrera = new ServicioCarrera();
 		
 		// Se crean y cargan carreras
@@ -34,11 +33,11 @@ public class Application {
 		//servicioEstudiante.altaEstudiante(null);
 
 		// a- Dar de alta estudiante		
-		//Estudiante e1 = new Estudiante(245852, "Juan", "Perez" , 25, "M", "39526555", "Tandil");
-		//Estudiante e2 = new Estudiante(268584, "Maria", "Gonzalez" , 22, "F", "42526555", "Mar del Plata");
+		Estudiante e1 = new Estudiante(245852, "Juan", "Perez" , 25, "M", "39526555", "Tandil");
+		Estudiante e2 = new Estudiante(268584, "Maria", "Gonzalez" , 22, "F", "42526555", "Mar del Plata");
 		
-		//servicioEstudiante.altaEstudiante(e1);
-		//servicioEstudiante.altaEstudiante(e2);
+		servicioEstudiante.altaEstudiante(e1);
+		servicioEstudiante.altaEstudiante(e2);
 		
 		// b- Matricular un estudiante a una carrera
 		//servicioestudiante.matricularEstudiante()
@@ -65,39 +64,39 @@ public class Application {
 		
 
 		// CÓDIGO DE PRUEBA PARA VERIFICAR QUE FUNCIONA LA BD
-		em.getTransaction().begin();
-		Estudiante e1 = em.find(Estudiante.class, 245852);
-		if (e1 == null) {
-			e1 = new Estudiante(245852, "Juan", "Perez" , 25, "M", "39526555", "Tandil");
-			em.persist(e1);			
-		}
-		Estudiante e2 = em.find(Estudiante.class, 245953);
-		if (e2 == null) {
-			e2 = new Estudiante(245953, "Maria", "Rodriguez" , 31, "F", "36222544", "Tandil");
-			em.persist(e2);			
-		}
+		// em.getTransaction().begin();
+//		Estudiante e1 = em.find(Estudiante.class, 245852);
+//		if (e1 == null) {
+//			e1 = new Estudiante(245852, "Juan", "Perez" , 25, "M", "39526555", "Tandil");
+//			em.persist(e1);			
+//		}
+//		Estudiante e2 = em.find(Estudiante.class, 245953);
+//		if (e2 == null) {
+//			e2 = new Estudiante(245953, "Maria", "Rodriguez" , 31, "F", "36222544", "Tandil");
+//			em.persist(e2);			
+//		}
 
-		Carrera c1 = em.find(Carrera.class, 1);
-		if (c1 == null) {
-			c1 = new Carrera(1, "sistemas");
-			em.persist(c1);
-		}
-		Carrera c2 = em.find(Carrera.class, 2);
-		if (c2 == null) {
-			c2 = new Carrera(2, "tudai");
-			em.persist(c2);
-		}
-		Inscripcion i1 = em.find(Inscripcion.class, new InscripcionId(e1, c1));
-		if (i1 == null) {
-			i1 = new Inscripcion(e1, c1);
-			em.persist(i1);
-		}
-		Inscripcion i2 = em.find(Inscripcion.class, new InscripcionId(e1, c2));
-		if (i2 == null) {
-			i1 = new Inscripcion(e1, c2);
-			em.persist(i1);
-		}
-		em.getTransaction().commit();
+		// Carrera c1 = em.find(Carrera.class, 1);
+		// if (c1 == null) {
+		// 	c1 = new Carrera(1, "sistemas");
+		// 	em.persist(c1);
+		// }
+		// Carrera c2 = em.find(Carrera.class, 2);
+		// if (c2 == null) {
+		// 	c2 = new Carrera(2, "tudai");
+		// 	em.persist(c2);
+		// }
+		// Inscripcion i1 = em.find(Inscripcion.class, new InscripcionId(e1, c1));
+		// if (i1 == null) {
+		// 	i1 = new Inscripcion(e1, c1);
+		// 	em.persist(i1);
+		// }
+		// Inscripcion i2 = em.find(Inscripcion.class, new InscripcionId(e1, c2));
+		// if (i2 == null) {
+		// 	i1 = new Inscripcion(e1, c2);
+		// 	em.persist(i1);
+		// }
+		// em.getTransaction().commit();
 	
 	}
 
