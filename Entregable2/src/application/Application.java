30812e19f981a2bf3174beadb7624cad841cfacb
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2023-09-23 14:04:21 -0300

    borrar codigo comentado en main

--- Entregable2/src/application/Application.java
+++ Entregable2/src/application/Application.java
@@ -62,43 +62,6 @@ public class Application {
 		
 		//ejercicio 3
 		servicioCarrera.generarReporteCarrera();
-		
-
-		// CÓDIGO DE PRUEBA PARA VERIFICAR QUE FUNCIONA LA BD
-		// em.getTransaction().begin();
-//		Estudiante e1 = em.find(Estudiante.class, 245852);
-//		if (e1 == null) {
-//			e1 = new Estudiante(245852, "Juan", "Perez" , 25, "M", "39526555", "Tandil");
-//			em.persist(e1);			
-//		}
-//		Estudiante e2 = em.find(Estudiante.class, 245953);
-//		if (e2 == null) {
-//			e2 = new Estudiante(245953, "Maria", "Rodriguez" , 31, "F", "36222544", "Tandil");
-//			em.persist(e2);			
-//		}
-
-		// Carrera c1 = em.find(Carrera.class, 1);
-		// if (c1 == null) {
-		// 	c1 = new Carrera(1, "sistemas");
-		// 	em.persist(c1);
-		// }
-		// Carrera c2 = em.find(Carrera.class, 2);
-		// if (c2 == null) {
-		// 	c2 = new Carrera(2, "tudai");
-		// 	em.persist(c2);
-		// }
-		// Inscripcion i1 = em.find(Inscripcion.class, new InscripcionId(e1, c1));
-		// if (i1 == null) {
-		// 	i1 = new Inscripcion(e1, c1);
-		// 	em.persist(i1);
-		// }
-		// Inscripcion i2 = em.find(Inscripcion.class, new InscripcionId(e1, c2));
-		// if (i2 == null) {
-		// 	i1 = new Inscripcion(e1, c2);
-		// 	em.persist(i1);
-		// }
-		// em.getTransaction().commit();
-	
 	}
 
 }
