a364a98b21577f2bb1e80e4af8aa744286ce87ec
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2023-10-01 12:01:05 -0300

    agrega Carrera Rest Controller

---
+++ Entregable3/src/main/java/com/example/entregable3/controller/CarreraController.java
@@ -0,0 +1,62 @@
+package com.example.entregable3.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entregable3.exception.ExpectableException;
+import com.example.entregable3.model.Carrera;
+import com.example.entregable3.service.CarreraServicio;
+
+
+@RestController
+@RequestMapping("/carreras")
+public class CarreraController {
+
+	@Autowired
+	private CarreraServicio carreraServicio;
+
+
+    @GetMapping({"", "/"})
+    public ResponseEntity<?>  getAll() {
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(carreraServicio.findAll());
+        }catch (ExpectableException e){
+        	return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?>getOne(@PathVariable Long id){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(carreraServicio.findById(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. No se encuentra el objeto buscado" +
+                    ".\"}");
+        }
+    }
+	
+	@PostMapping({"", "/"})
+	public ResponseEntity<?> save(@RequestBody Carrera carrera){
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(this.carreraServicio.save(carrera));
+		} catch (Exception e2) {
+			System.out.println("error " + e2.getMessage());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+		}
+	}
+
+}
