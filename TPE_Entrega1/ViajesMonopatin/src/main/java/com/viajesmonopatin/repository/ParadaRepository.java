11277dd91ff8576bba29a9334a16a5022b8589e9
Magui <magalisuyaygarrido@gmail.com>
2023-10-28 23:33:51 -0300

    MS viajesMonopatin , inicio viaje

--- TPE_Entrega1/ViajesMonopatin/src/main/java/com/viajesmonopatin/repository/ParadaRepository.java
+++ TPE_Entrega1/ViajesMonopatin/src/main/java/com/viajesmonopatin/repository/ParadaRepository.java
@@ -9,7 +9,8 @@ import com.viajesmonopatin.model.Parada;
 public interface ParadaRepository extends RepoBase<Parada, Long>{
 	static List<String> sorteableFields = List.of("id", "latitud","longitud","direccion");
 	
-	@Query("SELECT p FROM Parada p WHERE p.latitud = :latitud AND p.longitud = :longitud")
+	@Query("SELECT p FROM Parada p WHERE p.latitud BETWEEN :latitud - 1.0 AND :latitud + 1.0 "
+		    + "AND p.longitud BETWEEN :longitud - 1.0 AND :longitud + 1.0")
 	Parada getByLatitudLongitud(Float latitud, Float longitud);
 
 }
